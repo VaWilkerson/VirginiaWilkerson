fd12fab4b015349efa9a4be7080e3b3893976bfc
virginiawilkerson <vbw209@nyu.edu>
2019-10-11 17:58:04 -0400

    Chapin helped me actually get it working.

---
+++ Blackjack/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript
+    : MonoBehaviour
+{
+    public AudioClip fartSound;
+    private AudioSource audioSource; 
+        
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        //looking at AudioSource component on this object. Telling it to assign the AudioSource to the variable audioSource. 
+        //Now anytime I want to reference the AudioSource I can sue the variable audioSource to reference it.  
+        audioSource = GetComponent<AudioSource>();
+        // == is checking, = is setting. 
+    }
+
+
+    public void PlayFartSound()
+    {
+        audioSource.PlayOneShot(fartSound);//PlayOneShot nice to play something... once. 
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
