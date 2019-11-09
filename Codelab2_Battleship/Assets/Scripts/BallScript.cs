a6b25dd81af7023a04eec51f1c5d05996be4ab5f
virginiawilkerson <vbw209@nyu.edu>
2019-11-08 20:22:53 -0500

    this is as far as I got.

--- Codelab2_Battleship/Assets/Scripts/BallScript.cs
+++ Codelab2_Battleship/Assets/Scripts/BallScript.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
@@ -10,7 +11,9 @@ public class BallScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2(5,3 );
+        
+        //rb.AddForce(Vector2.up * 6);
+        //rb.velocity = new Vector2(5,3 );
         //I dont understand why this has to be a new vector 2. 
     }
 
@@ -20,11 +23,13 @@ public class BallScript : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other) //I guess 'other' is the other collider? it lets me rename it so idk.
     {
-        if (other.transform.CompareTag("Brick"))
+        if (other.transform.CompareTag("Brick"))    
         {
             Destroy(other.gameObject);
         }
+        
+        //if ()
     }
 }
