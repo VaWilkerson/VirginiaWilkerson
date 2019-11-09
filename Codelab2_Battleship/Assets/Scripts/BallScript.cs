3fe810642c657163df92ffaba1ce473ded72649a
virginiawilkerson <vbw209@nyu.edu>
2019-11-08 19:37:20 -0500

    fuck if I know. I'm at the point where the internet is not helping.

--- Codelab2_Battleship/Assets/Scripts/BallScript.cs
+++ Codelab2_Battleship/Assets/Scripts/BallScript.cs
@@ -18,4 +18,7 @@ public class BallScript : MonoBehaviour
     {
         
     }
+   
+    
+    
 }
--- Codelab2_Battleship/Assets/Scripts/Tile.cs
+++ Codelab2_Battleship/Assets/Scripts/Tile.cs
@@ -6,11 +6,11 @@ using UnityEngine.UIElements;
 
 public class Tile : MonoBehaviour
 {
- 
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,12 +18,20 @@ public class Tile : MonoBehaviour
     {
         
     }
+    
+    
 
+    /*private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }*/
+
+    /*
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Brick")
+        if (collision.gameObject.tag == "Ball")
         {
             Destroy(this.gameObject);    //whelp this doesn't work as planned. 
         }
-    }
+    }*/
 }
